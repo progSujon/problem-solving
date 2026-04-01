34861cb76e6d5ded65d028324ebcba0cce7aa0e3
mdsujon-programmer <mdsujonblog009@gmail.com>
2026-04-01 14:25:49 +0600

    variable,array,function,object

---
+++ basic.js
@@ -0,0 +1,41 @@
+
+var friendList = ["alamin","adrita"];
+friendList.push("musa")
+friendList.push("hero alom")
+
+var costomer = {
+    name:"shourik",
+    age:18,
+    weight:180,
+    marriteStateus: false,
+}
+
+friendList.pop()
+friendList.pop()
+friendList.pop()
+let friend = "alamin";
+
+// let num1 = 5;
+// let num2 = 10;
+// let sum = num1+num2;
+// console.log(sum);
+
+// let num3 = 10;
+// let num4 = 20;
+// let sum2 = num3 + num4;
+// console.log(sum2);
+
+function sum(num1,num2){
+    a = num1+num2;
+    console.log(a);
+}
+
+sum(5,10);
+sum(5,20);
+sum(5,30);
+sum(5,40);
+sum(5,50);
+sum(5,60);
+
+
+
---
+++ readme.md
@@ -0,0 +1,8 @@
+
+
+*problem statment - 4 
+write a function that tells if a given number is even or odd 
+input: a number 
+return: even if number is evern or "odd" if the number is odd
+even:number when divided by 2 has zero reminder
+odd : number when divided by 2 has non zero (1,-1) remainder
